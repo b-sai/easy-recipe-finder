3bc419acd360cfd1d084537253599b1c2a81db61
b-sai <bsaishreyas@gmail.com>
2024-07-08 16:14:51 -0500

    Basic filter works

--- recipe_recommender/src/App.js
+++ recipe_recommender/src/App.js
@@ -1,10 +1,11 @@
 import Navbar from "./Navbar";
 import Home from "./Home";
-
+import Filter from "./Filter";
 function App() {
   return (
     <div>
       <Navbar />
+      <Filter />
       <Home />
     </div>
   );
---
+++ recipe_recommender/src/Filter.js
@@ -0,0 +1,80 @@
+import React, { useState } from "react";
+import {
+  Button,
+  Dialog,
+  DialogTitle,
+  DialogContent,
+  DialogActions,
+  Checkbox,
+  FormGroup,
+  FormControlLabel,
+} from "@mui/material";
+import { FilterList } from "@mui/icons-material";
+
+const FILTER_OPTIONS = [
+  { name: "beachfront", label: "Beachfront" },
+  { name: "wifi", label: "Wifi" },
+  { name: "kitchen", label: "Kitchen" },
+  { name: "freeParking", label: "Free parking" },
+  { name: "washer", label: "Washer" },
+  { name: "hotTub", label: "Hot tub" },
+];
+
+const FilterComponent = () => {
+  const [open, setOpen] = useState(false);
+  const [filters, setFilters] = useState(
+    Object.fromEntries(FILTER_OPTIONS.map(({ name }) => [name, false]))
+  );
+
+  const handleClickOpen = () => setOpen(true);
+  const handleClose = () => setOpen(false);
+
+  const handleFilterChange = (event) => {
+    const { name, checked } = event.target;
+    setFilters((prevFilters) => ({ ...prevFilters, [name]: checked }));
+  };
+
+  const handleApply = () => {
+    // Apply filter logic here
+    console.log("Applied filters:", filters);
+    handleClose();
+  };
+
+  return (
+    <div>
+      <Button
+        variant="outlined"
+        startIcon={<FilterList />}
+        onClick={handleClickOpen}
+      >
+        Filters
+      </Button>
+      <Dialog open={open} onClose={handleClose}>
+        <DialogTitle>Filters</DialogTitle>
+        <DialogContent>
+          <FormGroup>
+            {FILTER_OPTIONS.map(({ name, label }) => (
+              <FormControlLabel
+                key={name}
+                control={
+                  <Checkbox
+                    checked={filters[name]}
+                    onChange={handleFilterChange}
+                    name={name}
+                  />
+                }
+                label={label}
+              />
+            ))}
+          </FormGroup>
+        </DialogContent>
+        <DialogActions>
+          <Button onClick={handleClose}>Cancel</Button>
+          <Button onClick={handleApply}>Apply</Button>
+        </DialogActions>
+      </Dialog>
+    </div>
+  );
+};
+
+export default FilterComponent;
